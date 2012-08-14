4db27d1df728d0de9d97a171557adb811b2ad06d
Devinder Khroad <devinder.khroad@hp.com>
2012-08-14 10:21:14 -0700

    fixed twiki alias

--- dotbash/bash_profile_includes/twiki.sh
+++ dotbash/bash_profile_includes/twiki.sh
@@ -1,2 +1,7 @@
-alias twiki='open -a Firefox ~/TiddlyWiki/twiki.html'
-alias twikiw='open -a Firefox ~/TiddlyWiki/twikiw.html'
+if [ `uname` = 'Darwin' ]; then
+  alias twiki='open -a Firefox ~/TiddlyWiki/twiki.html'
+  alias twikiw='open -a Firefox ~/TiddlyWiki/twikiw.html'
+else 
+  alias twiki='firefox ~/TiddlyWiki/twiki.html &'
+  alias twikiw='firefox ~/TiddlyWiki/twikiw.html &'
+fi
