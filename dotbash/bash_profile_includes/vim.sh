c559c081794dfdaa88eeda056cdf7d8f8d82e2be
Devinder Khroad <devinder.khroad@hp.com>
2012-08-14 09:13:23 -0700

    added alias for mvim

--- dotbash/bash_profile_includes/vim.sh
+++ dotbash/bash_profile_includes/vim.sh
@@ -2,6 +2,7 @@ OS=`uname`
 if [ $OS = 'Darwin' ]; then
   alias  vims="mvim --servername FOOBAR"
   alias vimc="mvim --servername FOOBAR --remote"
+  alias vi="mvim -v"
 else
   alias  vims="gvim --servername FOOBAR"
   alias vimc="gvim --servername FOOBAR --remote"
