d0f6699b79624816a4acd0282c00e20a0bcdae53
Devinder Khroad <dkhroad@gmail.com>
2014-04-07 23:13:05 -0700

    script to setup symbolic links

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s ~/.gitconfig ./dotgitconfig
+ln -s ~/.githelpers ./dotgithelpers
+ln -s ~/.gvimrc ./dotvim/gvimrc
+ln -s ~/.vimrc  ./dotvim/vimrc
+ln -s ~/.vim  ./dotvim/vimr
+ln -s ~/.irbrc  ./dotirb/irbc
+ln -s ~/.zshrc ./dotzsh/dotzshrc # you will also need to clone oh-my-zsh repo
