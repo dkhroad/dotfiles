667d35b134ab3d39ec14c754b2e5a7cae81957c5
Devinder Khroad <dkhroad@gmail.com>
2014-04-08 02:42:06 -0700

    tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-ln -s ~/.gitconfig ./dotgitconfig
-ln -s ~/.githelpers ./dotgithelpers
-ln -s ~/.gvimrc ./dotvim/gvimrc
-ln -s ~/.vimrc  ./dotvim/vimrc
-ln -s ~/.vim  ./dotvim/vimr
-ln -s ~/.irbrc  ./dotirb/irbc
-ln -s ~/.zshrc ./dotzsh/dotzshrc # you will also need to clone oh-my-zsh repo
+ln -s  `pwd`/dotgitconfig ~/.gitconfig
+ln -s  `pwd`/dotgithelpers ~/.githelpers
+ln -s  `pwd`/dotvim/gvimrc ~/.gvimrc
+ln -s  `pwd`/dotvim/vimrc ~/.vimrc
+ln -s  `pwd`/dotvim/vimrc ~/.vim
+ln -s  `pwd`/dotirb/irbc  ~/.irbrc
+ln -s  `pwd`/dotzsh/dotzshrc ~/.zshrc # you will also need to clone oh-my-zsh repo
