8b8a4a981cd109869d67c87eeb39d2e066ff14fb
Devinder Khroad <dkhroad@gmail.com>
2017-05-15 18:13:29 -0700

    remove echo stmt

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ ln -s  `pwd`/dotirb/irbc  ~/.irbrc
 ln -s  `pwd`/dotzsh/dotzshrc ~/.zshrc # you will also need to clone oh-my-zsh repo
 ln -s  `pwd`/dotbash/bash_profile_includes ~/.bash_profile_includes
 if [[ ! -e ~/.oh-my-zsh ]]; then
-  echo git clone git://github.com/dkhroad/oh-my-zsh.git ~/.oh-my-zsh
+   git clone git://github.com/dkhroad/oh-my-zsh.git ~/.oh-my-zsh
 else
   echo ".oh-my-zsh is already cloned.."
 fi
