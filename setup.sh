8371b55ebae6130856e3e7786c09df328b59c2f0
Devinder Khroad <dkhroad@gmail.com>
2017-07-26 15:08:29 -0700

    Add easymotion and markdown plugins

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ ln -s  `pwd`/dotgitconfig ~/.gitconfig
 ln -s  `pwd`/dotgithelpers ~/.githelpers
 ln -s  `pwd`/dotvim/gvimrc ~/.gvimrc
 ln -s  `pwd`/dotvim/vimrc ~/.vimrc
-ln -s  `pwd`/dotvim/vimrc ~/.vim
 ln -s  `pwd`/dotirb/irbc  ~/.irbrc
 ln -s  `pwd`/dotzsh/dotzshrc ~/.zshrc # you will also need to clone oh-my-zsh repo
 ln -s  `pwd`/dotbash/bash_profile_includes ~/.bash_profile_includes
