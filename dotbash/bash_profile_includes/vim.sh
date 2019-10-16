00dfa60f69243a170bd260421da196fc4f18009c
Devinder Khroad <dkhroad@gmail.com>
2019-10-16 11:51:27 -0700

    changes to tmux/vim config files for WSL

--- dotbash/bash_profile_includes/vim.sh
+++ dotbash/bash_profile_includes/vim.sh
@@ -1,20 +1,16 @@
 OS=`uname`
 if [ $OS = 'Darwin' ]; then
-  alias  vims="vim --servername FOOBAR"
-  alias vimc="vim --servername FOOBAR --remote"
-  alias vi="vim"
+  alias vim="nvim"
+  alias vi="nvim"
+  export EDITOR=nvim
+
+  if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
+    export VISUAL="nvr --cc split --remote-wait +'set bufhidden=wipe'"
+  else 
+    export VISUAL=nvim
+  fi
 else
-  alias  vims="vim --servername FOOBAR"
-  alias vimc="vim --servername FOOBAR --remote"
   export TERM="xterm-256color"
+  export EDITOR=vim
 fi
 
-alias vim="nvim"
-alias vi="nvim"
-export EDITOR=nvim
-
-if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-  export VISUAL="nvr --cc split --remote-wait +'set bufhidden=wipe'"
-else 
-  export VISUAL=nvim
-fi
